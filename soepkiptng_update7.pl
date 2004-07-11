020a31d7ea6a01079af1b2b806f0f26cd6fd3972
Eric Lammerts <eric@lammerts.org>
2004-07-11 17:20:16 +0000

    add "filesize" field to song table fix playlist list fix playlist display in alllist

---
+++ soepkiptng_update7.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+############################################################################
+# soepkiptng (c) copyright 2000 Eric Lammerts <eric@lammerts.org>.
+# $Id$
+############################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as 
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# A copy of the GNU General Public License is available on the World Wide Web
+# at `http://www.gnu.org/copyleft/gpl.html'.  You can also obtain it by
+# writing to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+############################################################################
+
+use integer;
+use Cwd 'abs_path';
+use DBI;
+use Socket;
+use Getopt::Std;
+
+# find program directory
+$_ = $0;
+while(-l) {
+	my $l = readlink or die "readlink $_: $!\n";
+	if($l =~ m|^/|) { $_ = $l; } else { s|[^/]*$|/$l|; }
+}
+m|(.*)/|;
+my $progdir = abs_path($1);
+
+require "$progdir/soepkiptng.lib";
+
+getopts('c:');
+
+read_configfile(\%conf, $opt_c);
+
+$dbh = DBI->connect("DBI:$conf{db_type}:$conf{db_name}:$conf{db_host}",
+	$conf{db_user}, $conf{db_pass}) or die "can't connect to database";
+
+$files = $dbh->selectcol_arrayref("SELECT filename FROM song WHERE present AND filesize=0 AND filename LIKE '/%'");
+$num = 0;
+foreach(@$files) {
+	defined($size = -s $_) or next;
+	$dbh->do("UPDATE song SET filesize=? WHERE filename=?", undef, $size, $_)
+		or die "can't do sql command: " . $dbh->errstr . "\n";
+	$num++;
+}
+END {
+	print "$num records updated\n";
+}
